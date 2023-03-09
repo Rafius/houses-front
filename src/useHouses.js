7691e71551ac54fabbd09cbc132a2012e67bfe94
rafa <rafaelpuyol93@gmail.com>
2023-03-09 11:58:10 +0100

    Update api endpoint

--- src/useHouses.js
+++ src/useHouses.js
@@ -10,9 +10,9 @@ const useHouses = () => {
 
   useEffect(() => {
     const callHousesApi = async () => {
-      const { houses, count } = await fetchApi(
-        "https://houses-api.vercel.app/"
-      );
+      const { houses, count } = await fetchApi();
+      ("https://houses-api.vercel.app/getHouses");
+      // "http://localhost/getHouses"
       setHouses(houses);
       console.log("casas bajadas de precio", count);
     };
